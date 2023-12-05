6197006e06b03e648c7f1337b5df4ae76968bf93
Paweł Kubiak <pawel.kubiak.dev@gmail.com>
2023-12-05 20:15:43 +0100

    feat: routing example

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'a',
+    path: 'route-a',
     loadComponent: () => import('./components/page-a/page-a.component').then(c => c.PageAComponent)
   },
   {
-    path: 'b',
+    path: 'route-b',
     loadComponent: () => import('./components/page-b/page-b.component').then(c => c.PageBComponent)
   }
 ];
