1651761fac64d961cb79b902b931fe63945946c4
Paweł Kubiak <pawel.kubiak.dev@gmail.com>
2023-12-05 20:27:19 +0100

    feat: define redirect to route-a by default

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: "",
+		redirectTo: "route-a",
+		pathMatch: "full",
+  },
+  {
     path: 'route-a',
     loadComponent: () => import('./components/page-a/page-a.component').then(c => c.PageAComponent)
   },
